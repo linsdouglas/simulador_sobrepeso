d3708c2aabcd516d458cc447fe264bc07e19749c
Douglas da Silva Lins <lins.douglas@live.com>
2025-04-16 13:00:50 -0300

    doteenv

--- balanca/views.py
+++ balanca/views.py
@@ -5,6 +5,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
 import pymysql
+from dotenv import load_dotenv
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sap_file = os.path.join(BASE_DIR, 'data', 'dados_sap.xlsx')
@@ -16,6 +18,7 @@ df_sap = pd.read_excel(sap_file)
 df_sku = pd.read_excel(sku_file)
 df_sobrepeso = pd.read_excel(sobrepeso_file)
 df_expedicao = pd.read_excel(expedicao_file)
+load_dotenv(dotenv_path=os.path.join(BASE_DIR, 'databaseinfo.env'))
 
 import pandas as pd
 
