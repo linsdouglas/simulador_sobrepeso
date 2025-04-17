0c6968b54449cfd99c040d7c0574361c41d282cd
Douglas da Silva Lins <lins.douglas@live.com>
2025-04-17 19:47:51 -0300

    ajustes

--- simulador/settings.py
+++ simulador/settings.py
@@ -21,7 +21,7 @@ import logging.config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
-print("DB_HOST:", os.getenv('DB_HOST'))
+#print("DB_HOST:", os.getenv('DB_HOST'))
 
 
 
