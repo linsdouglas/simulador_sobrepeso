fee0bfeef16f54eecf8723e0c6f5ab5ca87d1ada
Douglas da Silva Lins <lins.douglas@live.com>
2025-04-17 17:58:52 -0300

    ajuste_url_balacnca

--- balanca/urls.py
+++ balanca/urls.py
@@ -5,6 +5,6 @@ from .views import upload_sap
 urlpatterns = [
     path('analise/', views.analise_ocorrencias, name='analise_ocorrencias'),
     path('api/expedicao/', views.receber_expedicao, name='receber_expedicao'),
-    path("upload_sap/", upload_sap),
+    path('api/upload_sap/', upload_sap, name='upload_sap'),
 ]
 
